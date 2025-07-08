2177409d9725cb22dc2f2ec9e2667710ed2123db
Spotatoking <1823ea@gmail.com>
2025-07-08 16:28:16 +0530

    Added Hopping

--- madcow-decease/scripts/Player_S.gd
+++ madcow-decease/scripts/Player_S.gd
@@ -11,7 +11,6 @@ var on_floor := true
 #health system
 @export var health =  100
 
-
 var speed_now = 5.0
 @export var speed_walk = 5.0
 @export var speed_sprint = 10.0
@@ -97,9 +96,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if  is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("jump"):
+			velocity.y *= 0.5
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
