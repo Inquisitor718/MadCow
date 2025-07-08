08238cc9317271edd0aab5b694b650b07091a9d8
Spotatoking <1823ea@gmail.com>
2025-07-08 16:28:16 +0530

    Added Hopping

--- madcow-decease/scripts/test_enemy.gd
+++ madcow-decease/scripts/test_enemy.gd
@@ -8,8 +8,9 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta):
 	pass
+	
 
 func Hit(dmg):
 	health -= dmg
