3aeb5f9b2f0cb69c7ddbd3ce29a085342588e8cd
Spotatoking <1823ea@gmail.com>
2025-08-14 19:38:40 +0530

    Rishi mkc

--- madcow-decease/scripts/Cows/explosion_Legacy.gd
+++ madcow-decease/scripts/Cows/explosion_Legacy.gd
@@ -4,14 +4,18 @@ extends Node3D
 @export var dmg_rad: float
 @export var exp_dur: float
 
+
+var collision_shape = CollisionShape3D.new()
+var shape = SphereShape3D.new()
+var area = Area3D.new()
+
 func _ready():
 	print("Boom spawn")
-	var area = Area3D.new()
+
 	add_child(area)
-	var collision_shape = CollisionShape3D.new()
-	var shape = SphereShape3D.new()
+	
 	collision_shape.shape = shape
-	shape.radius = lerp(shape.radius, dmg_rad, 0.1)
+	shape.radius = 0
 	area.add_child(collision_shape)
 	area.set_collision_layer(0)
 	area.set_collision_mask_value(2, true)
@@ -19,6 +23,8 @@ func _ready():
 	area.body_entered.connect(_on_body_entered)
 	await get_tree().create_timer(exp_dur).timeout
 	queue_free()
+func _process(delta: float) -> void:
+	shape.radius = lerp(shape.radius, dmg_rad, 0.1)
 
 
 func _on_body_entered(body):
