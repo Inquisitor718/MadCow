734eabf1f1ac443af10aab8803a0bb52040ca285
Inquisitor718 <shreyash.anand718@gmail.com>
2025-08-21 19:10:50 +0530

    commit69

---
+++ madcow-decease/Quit_button.gd
@@ -0,0 +1,5 @@
+extends Button
+@onready var node_2d: Node2D = $".."
+
+func _on_pressed() -> void:
+	node_2d.get_tree().quit()
---
+++ madcow-decease/basement.gd
@@ -0,0 +1,17 @@
+extends Node3D
+@onready var pause_menu: Control = $Pause_menu
+var paused= false
+
+func _process(delta):
+	if Input.is_action_just_pressed("Pause"):
+		pauseMenu()
+		
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	
+	paused =!paused
