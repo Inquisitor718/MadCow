d0cf164da5d693d26dc720a5b9679d988603df7c
Rishi-Gupta <guptarishiiitjee@gmail.com>
2025-08-22 22:27:08 +0530

    bkl

--- madcow-decease/scripts/Cows/explosion.gd
+++ madcow-decease/scripts/Cows/explosion.gd
@@ -14,5 +14,5 @@ func _on_area_3d_body_entered(body: CharacterBody3D) -> void:
 		_on_animation_player_animation_finished("Explosion")
 
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	queue_free()
